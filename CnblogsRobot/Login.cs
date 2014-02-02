f5c49f9c96ffaf8bf20744af8848925e119ec24a
FrankFan <fanyong@gmail.com>
2014-02-02 11:02:01 +0800

    add login logic code

---
+++ CnblogsRobot/Login.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CnblogsRobot
+{
+    /*
+     *  模拟登录逻辑代码
+     * 
+     */ 
+    public class Login
+    {
+
+    }
+}
