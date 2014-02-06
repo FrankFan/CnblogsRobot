3e9393cc3b871442b98cbd89fe4f7283541f532d
FrankFan <fanyong@gmail.com>
2014-02-06 10:31:49 +0800

    add ImageUploader.cs file

---
+++ CnblogsRobot/ImageUploader.cs
@@ -0,0 +1,22 @@
+﻿/*
+ *  Description：图片上传类
+ *  Author: FrankFan
+ *  Email:fanyong@gmail.com
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CnblogsRobot
+{
+    /*
+     * 1.请求url:http://upload.cnblogs.com/imageuploader/upload
+     * 2.需要登录，添加cookie
+     * 3.发起http请求，上传图片， Reponse中返回上传成功的图片地址。
+     */
+    public class ImageUploader
+    {
+
+    }
+}
